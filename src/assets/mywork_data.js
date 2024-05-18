a3831998c9a728f23a890426d0aa89b64ee2b220
Sahib Preet Singh <sahib61003@gmail.com>
2024-05-18 17:31:48 +0530

    Updated

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -10,13 +10,13 @@ const mywork_data = [
         w_no:1,
         w_name:"Data Analysis using Power BI",
         w_img:project1_img,
-        w_url: "https://github.com/Sahib61003/Order_Analysis_Using_PowerBI"
+        w_url: "https://github.com/Sahib61003/CodeClauseInternship_RoadLaneDetection"
     },
     {
         w_no:2,
         w_name:"Road Lane Detection Using CV",
         w_img:project2_img,
-        w_url: "https://github.com/Sahib61003/CodeClauseInternship_RoadLaneDetection"
+        w_url: "https://github.com/Sahib61003/Order_Analysis_Using_PowerBI"
     },
     {
         w_no:3,
