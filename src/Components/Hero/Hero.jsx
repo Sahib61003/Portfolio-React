2747e2fcf7de266f353621e8dd027bdc2f7e9170
Sahib Preet Singh <sahib61003@gmail.com>
2024-05-18 17:28:45 +0530

    Updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.png'
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.png';
+import resume from '../../assets/Resume.pdf';
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-        <img src = {profile_img} alt=""/>
-        <h1> <span> I am Sahib Preet Singh, </span> AI developer, based in India. OPEN TO WORK.</h1>
-        <p> I am a student at GGSIPU, pursuing B.Tech in Artificial Intelligence and Machine Learning. I am passionate about AI and I love to automate things around me. I am always curious to learn new things and I am always ready to take on new challenges. </p>
-        <div className='hero-action'>
-            <div className='hero-connect'> Connect with me</div>
-            <div className='hero-resume'> Download My Resume</div>
-        </div>
+      <img src={profile_img} alt="Profile" />
+      <h1><span>I am Sahib Preet Singh,</span> AI developer, based in India. OPEN TO WORK.</h1>
+      <p>I am a student at GGSIPU, pursuing B.Tech in Artificial Intelligence and Machine Learning. I am passionate about AI and I love to automate things around me. I am always curious to learn new things and I am always ready to take on new challenges.</p>
+      <div className='hero-action'>
+        <div className='hero-connect'>Connect with me</div>
+        <a href={resume} download="Resume.pdf" className='hero-resume'>Download My Resume</a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
