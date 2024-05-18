eab88c280db722acd0e86e01735321e3ad481561
Sahib Preet Singh <sahib61003@gmail.com>
2024-05-18 17:17:48 +0530

    Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,28 +8,28 @@ const Footer = () => {
   return (
     <div className="footer">
         <div className="footer-top">
-            <div className="footer-top-left">
+            <div className="footer-top-right">
                 <img src={logo} alt=''/>
                 <p> I am an AI enthusiast from India, currently pursuing Bachelors of Technology in AI and ML and will be graduating in 2025. Looking for a career start. </p>
             </div>
-            <div className="footer-top-right">
+            {/* <div className="footer-top-right">
                 <div className="footer-email-input">
                     <img src={user_icon} alt=""/>
                     <input type="email" placeholder="Enter your email"/>
                 </div>
                 <div className="footer-subscribe"> Subscribe </div>
-            </div>
+            </div> */}
         </div>
     <hr/>
     <div className="footer-bottom">
-        <div className="footer-bottom-left">
+        <div className="footer-bottom-right">
             <p> © 2024 All rights reserved </p>
         </div>
-        <div className="footer-bottom-right">
+        {/* <div className="footer-bottom-right">
             <p> Privacy Policy </p>
             <p> Terms of Service </p>
             <p> Connect with me </p>
-            </div>
+            </div> */}
         </div>
     </div>
   )
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -10,19 +10,19 @@ const mywork_data = [
         w_no:1,
         w_name:"Data Analysis using Power BI",
         w_img:project1_img,
-        w_url: "https://github.com/Sahib61003/Portfolio-React"
+        w_url: "https://github.com/Sahib61003/Order_Analysis_Using_PowerBI"
     },
     {
         w_no:2,
         w_name:"Road Lane Detection Using CV",
         w_img:project2_img,
-        w_url: "https://github.com/Sahib61003/Portfolio-React"
+        w_url: "https://github.com/Sahib61003/CodeClauseInternship_RoadLaneDetection"
     },
     {
         w_no:3,
         w_name:"EasyYatra Full Stack Website",
         w_img:project3_img,
-        w_url: "https://github.com/Sahib61003/Portfolio-React"
+        w_url: "https://github.com/Sahib61003/Easy_Yatra_Vihaan6.0"
     },
 ]
  
