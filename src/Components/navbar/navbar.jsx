c2e62efe9f3c660a2d3b6c865500217bd8c06439
Sahib Preet Singh <sahib61003@gmail.com>
2024-06-26 17:06:01 +0530

    Added

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,28 +1,59 @@
-import React, { useState } from 'react'
-import './navbar.css'
-import logo from '../../assets/logo.png'
-import underline from '../../assets/nav_underline.svg'
+import React, { useState } from 'react';
+import './navbar.css';
+import logo from '../../assets/logo.png';
+import underline from '../../assets/nav_underline.svg';
 
 const Navbar = () => {
     const [menu, setMenu] = useState("home");
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const handleMobileMenuToggle = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+    const closeMobileMenu = (menuItem) => {
+        setMenu(menuItem);
+        setIsMobileMenuOpen(false);
+    };
 
     return (
         <div className="navbar">
-            <img src={logo} alt=""/>
-            <label for="check" class="checkbtn">
-                <i class="fas fa-bars"></i>
-            </label>
-            <ul className='nav-menu'>
-                <li> <p onClick={()=>setMenu("home")}> Home </p> {menu==='Home'? <img src={underline} alt=''/>:<></> } </li>
-                <li> <a href='#about'> <p onClick={()=>setMenu("about me")}> About </p> </a> {menu==='About me'? <img src={underline} alt=''/>:<></> } </li>
-                <li> <a href='#services'> <p onClick={()=>setMenu("services")}> Services </p> </a> {menu==='Services'? <img src={underline} alt=''/>:<></> } </li>
-                <li> <a href='#work'> <p onClick={()=>setMenu("projects")}> Projects </p> </a>{ menu==='Projects'? <img src={underline} alt=''/>:<></> } </li>
-                <li> <a href='#contact'> <p onClick={()=>setMenu("contact")}> Contact </p> </a> {menu==='Contact'? <img src={underline} alt=''/>:<></> } </li>
-
+            <img src={logo} alt="logo"/>
+            <div className="checkbtn" onClick={handleMobileMenuToggle}>
+                <i className="fas fa-bars"></i>
+            </div>
+            <ul className={`nav-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+                <li> 
+                    <p onClick={() => closeMobileMenu('home')}> Home </p> 
+                    {menu === 'home' && <img src={underline} alt=''/>} 
+                </li>
+                <li> 
+                    <a href='#about'> 
+                        <p onClick={() => closeMobileMenu('about')}> About </p> 
+                    </a> 
+                    {menu === 'about' && <img src={underline} alt=''/>} 
+                </li>
+                <li> 
+                    <a href='#services'> 
+                        <p onClick={() => closeMobileMenu('services')}> Services </p> 
+                    </a> 
+                    {menu === 'services' && <img src={underline} alt=''/>} 
+                </li>
+                <li> 
+                    <a href='#work'> 
+                        <p onClick={() => closeMobileMenu('projects')}> Projects </p>
+                    </a> 
+                    {menu === 'projects' && <img src={underline} alt=''/>} 
+                </li>
+                <li> 
+                    <a href='#contact'> 
+                        <p onClick={() => closeMobileMenu('contact')}> Contact </p> 
+                    </a> 
+                    {menu === 'contact' && <img src={underline} alt=''/>} 
+                </li>
             </ul>
             <div className='nav-connect'> Connect with Me </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
